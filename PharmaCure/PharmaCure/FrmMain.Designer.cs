2049002dc47e4019901cbd450dc0076507e6a0d7
DESKTOP-I9TDAIE\Kreso <kresimirzelenika@gmail.com>
2017-04-25 21:33:05 +0200

    Dodane forme: FrmDodajZaposlenika, FrmDostava, FrmPopisLijekova, FrmKolicine, FrmNabava, FrmProdaja i Frm Recepti, te navigacija iz maina na navedene forme

--- PharmaCure/PharmaCure/FrmMain.Designer.cs
+++ PharmaCure/PharmaCure/FrmMain.Designer.cs
@@ -28,86 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnPrijaviSe = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnZaposlenici = new System.Windows.Forms.Button();
+            this.btnNabava = new System.Windows.Forms.Button();
+            this.btnPopisL = new System.Windows.Forms.Button();
+            this.btnDostava = new System.Windows.Forms.Button();
+            this.btnKolicina = new System.Windows.Forms.Button();
             this.btnIzlaz = new System.Windows.Forms.Button();
+            this.btnProdaja = new System.Windows.Forms.Button();
+            this.btnRecepti = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnPrijaviSe
+            // btnZaposlenici
             // 
-            this.btnPrijaviSe.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnPrijaviSe.Location = new System.Drawing.Point(107, 60);
-            this.btnPrijaviSe.Name = "btnPrijaviSe";
-            this.btnPrijaviSe.Size = new System.Drawing.Size(108, 95);
-            this.btnPrijaviSe.TabIndex = 1;
-            this.btnPrijaviSe.Text = "Prijavi se";
-            this.btnPrijaviSe.UseVisualStyleBackColor = true;
+            this.btnZaposlenici.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnZaposlenici.Location = new System.Drawing.Point(86, 39);
+            this.btnZaposlenici.Name = "btnZaposlenici";
+            this.btnZaposlenici.Size = new System.Drawing.Size(108, 95);
+            this.btnZaposlenici.TabIndex = 1;
+            this.btnZaposlenici.Text = "Dodaj Zaposlenika";
+            this.btnZaposlenici.UseVisualStyleBackColor = true;
+            this.btnZaposlenici.Click += new System.EventHandler(this.btnZaposlenici_Click);
             // 
-            // button1
+            // btnNabava
             // 
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(107, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 95);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Prijavi se";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnNabava.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNabava.Location = new System.Drawing.Point(86, 195);
+            this.btnNabava.Name = "btnNabava";
+            this.btnNabava.Size = new System.Drawing.Size(108, 95);
+            this.btnNabava.TabIndex = 2;
+            this.btnNabava.Text = "Nabava Lijekova";
+            this.btnNabava.UseVisualStyleBackColor = true;
+            this.btnNabava.Click += new System.EventHandler(this.btnNabava_Click);
             // 
-            // button2
+            // btnPopisL
             // 
-            this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(299, 60);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 95);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Prijavi se";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnPopisL.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPopisL.Location = new System.Drawing.Point(278, 39);
+            this.btnPopisL.Name = "btnPopisL";
+            this.btnPopisL.Size = new System.Drawing.Size(108, 95);
+            this.btnPopisL.TabIndex = 3;
+            this.btnPopisL.Text = "Popis Lijekova";
+            this.btnPopisL.UseVisualStyleBackColor = true;
+            this.btnPopisL.Click += new System.EventHandler(this.btnPopisL_Click);
             // 
-            // button3
+            // btnDostava
             // 
-            this.button3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(299, 206);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 95);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Prijavi se";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDostava.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDostava.Location = new System.Drawing.Point(278, 195);
+            this.btnDostava.Name = "btnDostava";
+            this.btnDostava.Size = new System.Drawing.Size(108, 95);
+            this.btnDostava.TabIndex = 4;
+            this.btnDostava.Text = "Dostava Lijekova";
+            this.btnDostava.UseVisualStyleBackColor = true;
+            this.btnDostava.Click += new System.EventHandler(this.btnDostava_Click);
             // 
-            // button4
+            // btnKolicina
             // 
-            this.button4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button4.Location = new System.Drawing.Point(491, 60);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(108, 95);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Prijavi se";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnKolicina.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnKolicina.Location = new System.Drawing.Point(470, 39);
+            this.btnKolicina.Name = "btnKolicina";
+            this.btnKolicina.Size = new System.Drawing.Size(108, 95);
+            this.btnKolicina.TabIndex = 5;
+            this.btnKolicina.Text = "Pregled Količine Lijekova";
+            this.btnKolicina.UseVisualStyleBackColor = true;
+            this.btnKolicina.Click += new System.EventHandler(this.btnKolicina_Click);
             // 
             // btnIzlaz
             // 
             this.btnIzlaz.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnIzlaz.Location = new System.Drawing.Point(491, 206);
+            this.btnIzlaz.Location = new System.Drawing.Point(470, 351);
             this.btnIzlaz.Name = "btnIzlaz";
-            this.btnIzlaz.Size = new System.Drawing.Size(108, 95);
+            this.btnIzlaz.Size = new System.Drawing.Size(108, 43);
             this.btnIzlaz.TabIndex = 6;
-            this.btnIzlaz.Text = "Izađi";
+            this.btnIzlaz.Text = "Odjavi se";
             this.btnIzlaz.UseVisualStyleBackColor = true;
             this.btnIzlaz.Click += new System.EventHandler(this.btnIzlaz_Click_1);
+            // 
+            // btnProdaja
+            // 
+            this.btnProdaja.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnProdaja.Location = new System.Drawing.Point(470, 195);
+            this.btnProdaja.Name = "btnProdaja";
+            this.btnProdaja.Size = new System.Drawing.Size(108, 95);
+            this.btnProdaja.TabIndex = 7;
+            this.btnProdaja.Text = "Prodaja Lijekova";
+            this.btnProdaja.UseVisualStyleBackColor = true;
+            this.btnProdaja.Click += new System.EventHandler(this.btnProdaja_Click);
+            // 
+            // btnRecepti
+            // 
+            this.btnRecepti.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnRecepti.Location = new System.Drawing.Point(278, 351);
+            this.btnRecepti.Name = "btnRecepti";
+            this.btnRecepti.Size = new System.Drawing.Size(108, 43);
+            this.btnRecepti.TabIndex = 8;
+            this.btnRecepti.Text = "Dodaj Recept";
+            this.btnRecepti.UseVisualStyleBackColor = true;
+            this.btnRecepti.Click += new System.EventHandler(this.btnRecepti_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(740, 386);
+            this.ClientSize = new System.Drawing.Size(706, 435);
+            this.Controls.Add(this.btnRecepti);
+            this.Controls.Add(this.btnProdaja);
             this.Controls.Add(this.btnIzlaz);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnPrijaviSe);
+            this.Controls.Add(this.btnKolicina);
+            this.Controls.Add(this.btnDostava);
+            this.Controls.Add(this.btnPopisL);
+            this.Controls.Add(this.btnNabava);
+            this.Controls.Add(this.btnZaposlenici);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmMain";
             this.Text = "FrmMain";
@@ -117,11 +148,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnPrijaviSe;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnZaposlenici;
+        private System.Windows.Forms.Button btnNabava;
+        private System.Windows.Forms.Button btnPopisL;
+        private System.Windows.Forms.Button btnDostava;
+        private System.Windows.Forms.Button btnKolicina;
         private System.Windows.Forms.Button btnIzlaz;
+        private System.Windows.Forms.Button btnProdaja;
+        private System.Windows.Forms.Button btnRecepti;
     }
 }
--- PharmaCure/PharmaCure/FrmMain.cs
+++ PharmaCure/PharmaCure/FrmMain.cs
@@ -24,5 +24,54 @@ namespace PharmaCure
             this.Close();
             
         }
+
+        private void btnZaposlenici_Click(object sender, EventArgs e)
+        {
+            FrmDodajZaposlenika z = new FrmDodajZaposlenika();
+            z.Show();
+            this.Close();
+        }
+
+        private void btnPopisL_Click(object sender, EventArgs e)
+        {
+            FrmPopisLijekova popis = new FrmPopisLijekova();
+            popis.Show();
+            this.Close();
+        }
+
+        private void btnKolicina_Click(object sender, EventArgs e)
+        {
+            FrmKolicine kol = new FrmKolicine();
+            kol.Show();
+            this.Close();
+        }
+
+        private void btnNabava_Click(object sender, EventArgs e)
+        {
+            FrmNabava nab = new FrmNabava();
+            nab.Show();
+            this.Close();
+        }
+
+        private void btnDostava_Click(object sender, EventArgs e)
+        {
+            FrmDostava dostava = new FrmDostava();
+            dostava.Show();
+            this.Close();
+        }
+
+        private void btnProdaja_Click(object sender, EventArgs e)
+        {
+            FrmProdaja prodaja = new FrmProdaja();
+            prodaja.Show();
+            this.Close();
+        }
+
+        private void btnRecepti_Click(object sender, EventArgs e)
+        {
+            FrmRecepti recepti = new FrmRecepti();
+            recepti.Show();
+            this.Close();
+        }
     }
 }
