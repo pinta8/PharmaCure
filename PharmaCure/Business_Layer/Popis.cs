5432a417d24b5240c5464670f9271b077b725233
pinta8 <danijel.pintaric8@gmail.com>
2018-01-24 16:34:19 +0100

    Popravak poslovnog sloja.

---
+++ PharmaCure/Business_Layer/Popis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    class Popis
+    {
+        public int RezervacijaID { get; set; }
+        public int LijekID { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
