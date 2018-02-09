0dc4594a66262ce46a2f86c1704ddddd5f641c8f
pinta8 <danijel.pintaric8@gmail.com>
2018-02-09 14:35:18 +0100

    poslovni sloj

---
+++ PharmaCure/Business_Layer/TipZaposlenika.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    class TipZaposlenika
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+    }
+}
